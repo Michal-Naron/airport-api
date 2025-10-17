5fb7ba509db957f1174cf74983bcc8e299d6ff5f
Michal-Naron <michanaron@gmail.com>
2025-10-17 02:14:04 +0200

    chore(settings): set pagination to 5 items per page for whole project

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -133,7 +133,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'users.permissions.IsAdminOrReadOnly',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
 }
 
 SIMPLE_JWT = {
