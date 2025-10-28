5342345aa90c9db7e05ba7d0b29e973006ed2e6e
Michal-Naron <michanaron@gmail.com>
2025-10-28 21:51:52 +0100

    chore(airport-api): add ALLOWED_HOSTD

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "0.0.0.0"]
 
 
 # Application definition
