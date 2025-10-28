7699a70b74ab0d67f518e7f7832b8663d59d1568
Michal-Naron <michanaron@gmail.com>
2025-10-28 20:43:35 +0100

    chore(airport-api): add load_dotenv to load environment variables

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import sys
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
