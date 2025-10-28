5faa77c70f957c346964b739d6683b81785c7d58
Michal-Naron <michanaron@gmail.com>
2025-10-28 21:30:19 +0100

    feat(airport-api): add Swagger API documentation

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'flights',
-    'users'
+    'users',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- airport_api/urls.py
+++ airport_api/urls.py
@@ -17,10 +17,28 @@ Including another URLconf
 
 # from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Airport API",
+      default_version='v1',
+      description="API documentation for Airport Management System",
+      terms_of_service="https://www.example.com/terms/",
+      contact=openapi.Contact(email="contact@example.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-       # path('admin/', admin.site.urls),
     path('api/v1/', include("flights.urls", namespace="flights")),
-    path('api/v1/user/', include("users.urls", namespace="users"))
+    path('api/v1/user/', include("users.urls", namespace="users")),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
 
 ]
